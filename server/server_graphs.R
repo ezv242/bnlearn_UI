f2b2a2a480f925a14601ff3cf0d460acca337ca2
Ernesto Zenteno Vargas <ezv242@inlumine.ual.es>
2026-04-13 17:21:51 +0200

    grafo
    reactivo

--- server/server_graphs.R
+++ server/server_graphs.R
@@ -1,9 +1,10 @@
 server_graphs <- function(input, output, session, shared_data) {
 
+  # Reactive para almacenar nodos y aristas del grafo 
   nodes <- reactiveVal(NULL)
   edges <- reactiveVal(NULL)
 
-  graph_results <- observeEvent(input$run_bnlearn, {
+  graph_results <- observeEvent(shared_data$network, {
     req(shared_data$network)
     bn <- shared_data$network
 
@@ -11,29 +12,57 @@ server_graphs <- function(input, output, session, shared_data) {
       id = bnlearn::nodes(bn),
       label = bnlearn::nodes(bn)
     ))
-    edges(as.data.frame(bnlearn::arcs(bn)))
-  })
+
+    arcs_df <- as.data.frame(bnlearn::arcs(bn))
+    if (nrow(arcs_df) == 0) {
+      arcs_df <- data.frame(from = character(0), to = character(0), stringsAsFactors = FALSE)
+    }
+    edges(arcs_df)
+  }, ignoreNULL = TRUE)
 
   # Render del grafo
   output$graph <- renderVisNetwork({
     req(nodes(), edges())
-    visNetwork(nodes(), edges(), height = "100%", width = "100%") %>%
+    visNetwork(nodes(), edges(), height = "50%", width = "100%") %>%
     visNodes(shape = "dot", size = 20) %>%
     visEdges(arrows = "to") %>%
     visOptions(manipulation = TRUE)
   })
 
-  # Agregar nodo
-  observeEvent(input$add_node, {
-    new_id <- nrow(nodes()) + 1
-    nodes(rbind(nodes(), data.frame(id = new_id, label = input$new_node)))
+  # Sincronizar cambios visuales con el modelo bnlearn
+  observeEvent(list(input$graph_nodes, input$graph_edges), {
+    req(input$graph_nodes)
+
+    nodes_df <- input$graph_nodes
+    edges_df <- input$graph_edges
+    if (is.null(edges_df)) {
+      edges_df <- data.frame(from = character(0), to = character(0), stringsAsFactors = FALSE)
+    }
+
+    # Crear grafo vacío con los nodos seleccionados
+    bn_new <- bnlearn::empty.graph(nodes_df$id)
+
+    # Añadir aristas si hay
+    if (nrow(edges_df) > 0) {
+      arcs_matrix <- as.matrix(edges_df[, c("from", "to")])
+      bnlearn::arcs(bn_new) <- arcs_matrix
+    }
+
+    # Guardar en el shared_data
+    shared_data$network <- bn_new
+
+    # Actualizar nodos y edges visibles para mantener coherencia
+    nodes(nodes_df)
+    edges(edges_df)
   })
 
-  # Eliminar último nodo
-  observeEvent(input$remove_node, {
-    current_nodes <- nodes()
-    if(nrow(current_nodes) > 0) {
-      nodes(current_nodes[-nrow(current_nodes),])
-    }
+  output$bn_debug <- renderPrint({
+    req(shared_data$network)
+
+    cat("Nodos:\n")
+    print(bnlearn::nodes(shared_data$network))
+
+    cat("\nArcos:\n")
+    print(bnlearn::arcs(shared_data$network))
   })
 }
--- ui/ui_results.R
+++ ui/ui_results.R
@@ -7,12 +7,14 @@ ui_results <- function() {
     sidebarLayout(
       sidebarPanel(
         textInput("new_node", "Nuevo nodo"),
-        actionButton("add_node", "Agregar nodo"),
-        actionButton("remove_node", "Eliminar último nodo")
+        #actionButton("add_node", "Agregar nodo"),
+        #actionButton("remove_node", "Eliminar último nodo")
       ),
       mainPanel(
         #visNetworkOutput("graph")
-        div(style = "height: 800px;", visNetworkOutput("graph", height = "100%"))
+        div(style = "height: 800px;", visNetworkOutput("graph", height = "50%")),
+        h4("Red bayesiana actual:"),
+        verbatimTextOutput("bn_debug")   # <- Aquí se mostrará la red
       )
     ),
     verbatimTextOutput("inference_output"),
